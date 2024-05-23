3eb29748bff239971235bb5bb4203f39d0c56c47
SaltAndViChips <brandons123@live.ca>
2024-05-22 23:25:49 -0300

    vault backup: 2024-05-22 23:25:48

---
+++ quartz/plugins/filters/noindex.ts
@@ -0,0 +1,14 @@
+const explorerFilterFn = (node: FileNode) => {
+  return !["tags", "noindex"].some((path) => node.name.includes(path))
+}
+export const defaultListPageLayout: PageLayout = {
+  beforeBody: [Component.ArticleTitle()],
+  left: [
+    Component.Meta({ enableSearch: false, enableDarkMode: false }),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Search(),
+    Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer({ filterFn: explorerFilterFn })),
+  ],
+  right: [],
+}
