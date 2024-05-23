7e0e6bcaac0bd5b13938c800c7f1a83e3b4150dc
SaltAndViChips <brandons123@live.ca>
2024-05-22 23:36:18 -0300

    vault backup: 2024-05-22 23:36:18

--- quartz/plugins/filters/index.ts
+++ quartz/plugins/filters/index.ts
@@ -1,5 +1,3 @@
 export { RemoveDrafts } from "./draft"
 export { ExplicitPublish } from "./explicit"
-export { explorerFilterFn } from  "./noindex"
-export { defaultListPageLayout } from  "./noindex"
 
